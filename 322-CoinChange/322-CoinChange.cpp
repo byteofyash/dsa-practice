9d87a7e03fead8050578e25c944415d5ede4f584
Yash Ranjan <yashranjan700@gmail.com>
2026-07-07 02:40:17 +0530

    Create 322-CoinChange

---
+++ 322-CoinChange/322-CoinChange.cpp
@@ -0,0 +1,19 @@
+// Last updated: 7/7/2026, 2:40:15 AM
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, 1e9);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (auto coin : coins) {
+                if (i - coin >= 0) {
+                    dp[i] = min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        if (dp[amount] < 1e9)
+            return dp[amount];
+        else
+            return -1;
+    }
+};
