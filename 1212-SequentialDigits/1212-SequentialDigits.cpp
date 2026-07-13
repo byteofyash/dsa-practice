d79c217ddb41b2d706f13304c4e475412aba2f64
Yash Ranjan <yashranjan700@gmail.com>
2026-07-14 03:42:22 +0530

    Create 1212-SequentialDigits

---
+++ 1212-SequentialDigits/1212-SequentialDigits.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/14/2026, 3:42:22 AM
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        string s  = "123456789";
+        vector<int> result;
+        for(int len = 2; len<=9 ; len++){
+            for(int i  = 0 ; i<=9-len;i++){
+                int num = stoi(s.substr(i,len));
+                if(num>=low && num<=high){
+                    result.push_back(num);
+                }
+            }
+        }
+
+        sort(result.begin(), result.end());
+        return result;
+        
+    }
+};
