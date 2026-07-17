2104f19be1f8734c0260043da2fc2129403ec466
Yash Ranjan <yashranjan700@gmail.com>
2026-07-17 19:47:40 +0530

    Create 714-BestTimeToBuyAndSellStockWithTransactionFee

---
+++ 714-BestTimeToBuyAndSellStockWithTransactionFee/714-BestTimeToBuyAndSellStockWithTransactionFee.cpp
@@ -0,0 +1,42 @@
+// Last updated: 7/17/2026, 7:47:40 PM
+class Solution {
+int n;
+int f;
+
+    int solve(int day, int canBuy, vector<int>& prices,
+              vector<vector<int>>& dp) {
+        if (day == n)
+            return 0;
+
+        if (dp[day][canBuy] != -1)
+            return dp[day][canBuy];
+        
+        int ans;
+
+        if (canBuy) {
+            // buy
+            int buy = -prices[day] + solve(day + 1, 0, prices, dp);
+            // skip
+            int skip = solve(day + 1, 1, prices, dp);
+            ans = max(buy, skip);
+        }
+
+        else {
+            // sell
+            int sell = (prices[day] + solve(day + 1, 1, prices, dp))-f;
+            // skip
+            int skip = solve(day + 1, 0, prices, dp);
+            ans = max(sell, skip);
+        }
+
+        dp[day][canBuy] = ans;
+        return dp[day][canBuy];
+    }
+
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+         n = prices.size(); f = fee;
+        vector<vector<int>> dp(n, vector<int>(2, -1));
+        return solve(0, 1, prices, dp);
+    }
+};
