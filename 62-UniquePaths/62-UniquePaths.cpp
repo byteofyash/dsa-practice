c0f91a61ff2d5ce357d96f96cf997a0603c9cc7b
Yash Ranjan <yashranjan700@gmail.com>
2026-07-31 23:23:31 +0530

    Create 62-UniquePaths

---
+++ 62-UniquePaths/62-UniquePaths.cpp
@@ -0,0 +1,18 @@
+// Last updated: 7/31/2026, 11:23:31 PM
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        dp[0][0] = 1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i==0 && j==0) continue;
+                int up = (i > 0) ? dp[i - 1][j] : 0;
+                int left = (j > 0) ? dp[i][j - 1] : 0;
+                dp[i][j] = up + left;
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
