c4893328cba1c06b0761bd77898a228b1db0dc8c
Yash Ranjan <yashranjan700@gmail.com>
2026-08-02 23:22:30 +0530

    Create 909-StoneGame

---
+++ 909-StoneGame/909-StoneGame.cpp
@@ -0,0 +1,25 @@
+// Last updated: 8/2/2026, 11:22:29 PM
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        int n = piles.size();
+        vector<vector<int>> dp(n, vector<int>(n));
+
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = piles[i];
+        }
+
+        for (int len = 2; len <= n; len++) {
+            for (int left = 0; left + len - 1 < n; left++) {
+                int right = left + len - 1;
+
+                int takeLeft = piles[left] - dp[left + 1][right];
+                int takeRight = piles[right] - dp[left][right - 1];
+
+                dp[left][right] = max(takeLeft, takeRight);
+            }
+        }
+
+        return dp[0][n - 1] > 0;
+    }
+};
