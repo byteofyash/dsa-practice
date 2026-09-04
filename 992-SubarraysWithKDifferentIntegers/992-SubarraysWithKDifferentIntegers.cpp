b617701d65554ba1ef069af4b88d9640aa806f89
Yash Ranjan <yashranjan700@gmail.com>
2026-09-04 16:58:14 +0530

    Create 992-SubarraysWithKDifferentIntegers

---
+++ 992-SubarraysWithKDifferentIntegers/992-SubarraysWithKDifferentIntegers.cpp
@@ -0,0 +1,27 @@
+// Last updated: 9/4/2026, 4:58:13 PM
+class Solution {
+    int atMost(vector<int>& nums, int k) {
+        int n = nums.size();
+        int l = 0, r = 0;
+        unordered_map<int, int> freq;
+        int ans = 0;
+        while (r < n) {
+            freq[nums[r]]++;
+            while (freq.size() > k) {
+                freq[nums[l]]--;
+
+                if (freq[nums[l]] == 0)
+                    freq.erase(nums[l]);
+                l++;
+            }
+            ans += r - l + 1;
+            r++;
+        }
+        return ans;
+    }
+
+public:
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return atMost(nums, k) - atMost(nums, k - 1);
+    }
+};
