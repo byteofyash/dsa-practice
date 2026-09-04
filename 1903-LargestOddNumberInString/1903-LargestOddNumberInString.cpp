873ddbbfff1b2c24893522c8491b42a736bf08c6
Yash Ranjan <yashranjan700@gmail.com>
2026-09-04 16:54:22 +0530

    Create 1903-LargestOddNumberInString

---
+++ 1903-LargestOddNumberInString/1903-LargestOddNumberInString.cpp
@@ -0,0 +1,13 @@
+// Last updated: 9/4/2026, 4:54:20 PM
+class Solution {
+public:
+    string largestOddNumber(string num) {
+
+        for(int i = num.size()-1;i>=0;i--){
+            if((num[i]-'0')%2!=0) return num.substr(0,i+1);
+        }
+
+        return "";
+        
+    }
+};
