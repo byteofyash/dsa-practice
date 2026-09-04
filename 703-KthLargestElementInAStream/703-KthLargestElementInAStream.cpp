36d6bf3b18e121351b5c2f4578385e5b8a3cb04f
Yash Ranjan <yashranjan700@gmail.com>
2026-09-04 16:59:35 +0530

    Create 703-KthLargestElementInAStream

---
+++ 703-KthLargestElementInAStream/703-KthLargestElementInAStream.cpp
@@ -0,0 +1,26 @@
+// Last updated: 9/4/2026, 4:59:34 PM
+class KthLargest {
+public:
+    int n;
+    priority_queue<int, vector<int>, greater<int>> pq;
+    KthLargest(int k, vector<int>& nums) {
+        n = k;
+        for(auto &x : nums){
+            add(x);
+        }
+    }
+    
+    int add(int val) {
+        pq.push(val);
+        if(pq.size()>n){
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
