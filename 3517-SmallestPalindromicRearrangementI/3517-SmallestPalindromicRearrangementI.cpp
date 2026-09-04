8a663ea6f54e91da345feefe11e9f29d4a1c02ac
Yash Ranjan <yashranjan700@gmail.com>
2026-09-04 16:51:44 +0530

    Create 3517-SmallestPalindromicRearrangementI

---
+++ 3517-SmallestPalindromicRearrangementI/3517-SmallestPalindromicRearrangementI.cpp
@@ -0,0 +1,33 @@
+// Last updated: 9/4/2026, 4:51:44 PM
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        int freq[26] = {};
+
+        for (char c : s) {
+            freq[c - 'a']++;
+        }
+
+        string half;
+        char middle = 0;
+
+        for (int i = 0; i < 26; i++) {
+            half += string(freq[i] / 2, 'a' + i);
+
+            if (freq[i] % 2) {
+                middle = 'a' + i;
+            }
+        }
+
+        string ans = half;
+
+        if (middle) {
+            ans += middle;
+        }
+
+        reverse(half.begin(), half.end());
+        ans += half;
+
+        return ans;
+    }
+};
