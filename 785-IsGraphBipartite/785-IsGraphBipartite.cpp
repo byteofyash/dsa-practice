9fc865bb575693d18f0a77841f35548e313624df
Yash Ranjan <yashranjan700@gmail.com>
2026-09-04 16:59:21 +0530

    Create 785-IsGraphBipartite

---
+++ 785-IsGraphBipartite/785-IsGraphBipartite.cpp
@@ -0,0 +1,30 @@
+// Last updated: 9/4/2026, 4:59:20 PM
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+
+        vector<int> color(n, -1);
+        for (int i = 0; i < n; i++) {
+            if(color[i]!=-1) continue;
+            queue<int> q;
+            q.push(i);
+            color[i] = 0;
+
+            while (!q.empty()) {
+                int u = q.front();
+                q.pop();
+                for (auto& v : graph[u]) {
+                    if (color[v] == -1) {
+                        color[v] = 1 - color[u];
+                        q.push(v);
+                    } else if (color[v] == color[u]) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+};
